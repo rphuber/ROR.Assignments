d2cac1b8a9b815cb82b6a466e405ce9eefc49547
Ryan Huber <ryan.huber@mac.com>
2015-01-20 00:52:09 -0500

    add crud

--- 09.Blog/09.blog.rb
+++ 09.Blog/09.blog.rb
@@ -25,3 +25,50 @@ get '/' do
 	@posts = Post.all
 	erb :index, layout: :layout
 end
+
+# New
+get '/new' do
+	@page = "My Blog - New Post"
+	erb :new_post, layout: :layout
+end
+
+#Create
+post '/create_post' do
+	p params
+	@post = Post.new
+	@post.title = params[:title]
+	@post.author = params[:author]
+	@post.content = params[:content]
+	@post.save
+	redirect to '/'
+end
+
+# Show
+get '/post/:id' do
+	@page = "My Blog - Post Detail"
+	@post = Post.get params[:id]
+	erb :post_detail, layout: :layout
+end
+
+# Edit
+get '/edit/:id' do
+	@page = "My Blog - Edit Post"
+	@post = Post.get params[:id]
+	erb :post_edit, layout: :layout
+end
+
+# Update
+patch '/update_post/:id' do
+	@post = Post.get params[:id]
+	@post.update title:params[:title]
+	@post.update author:params[:author]
+	@post.update content:params[:content]
+	redirect to '/'
+end
+
+# Delete
+delete '/delete_post/:id' do
+	@post = Post.get params[:id]
+	@post.destroy
+	redirect to '/'
+end
